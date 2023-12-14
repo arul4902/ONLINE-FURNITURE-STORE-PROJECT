cac08019c48c06ae16891de3072e807123d7aac2
arul4902 <kumarsiva42887@gmail.com>
2023-12-14 09:19:00 +0530

    inital commmit

--- furniturestore/src/App.js
+++ furniturestore/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
     <h1>STOCK  MANAGEMENT  SYSTEM </h1>
-    {/* <Login/> */}
+    
       <BrowserRouter>
     <Navigate></Navigate>
         <Routes>
